24c8f3e2dad0b3370e40aa7d25c6b2b31ab7952b
ACCEPT786 <444709834@qq.com>
2019-12-18 14:48:34 +0800

    commit

--- auto_connection.py
+++ auto_connection.py
@@ -18,16 +18,16 @@ def check_and_connect():
     while True:
         return1 = os.system('ping -c2 www.baidu.com')
         if return1:
-            username_str = "2125919"  # 你的校园网登陆用户名
-            password_str = "135798642CY"  # 你的校园网登陆密码
+            username_str = "***"  # 你的校园网登陆用户名
+            password_str = "***"  # 你的校园网登陆密码
             driver = webdriver.Firefox(executable_path=current_path + "/geckodriver")
             can_connect = True
             driver.get("http://login.wku.edu.cn/ac_portal/default/pc.html?tabs=pwd")  # 你的校园网登陆地址
             time.sleep(3)
-            username_input = driver.find_element_by_id("password_name")  # 校园网登陆用户名的输入控件ID, 浏览器上右键查看网页源代码查询
-            password_input = driver.find_element_by_id("password_pwd")  # 校园网登陆密码的输入控件ID, 浏览器上右键查看网页源代码查询
+            username_input = driver.find_element_by_id("password_name")
+            password_input = driver.find_element_by_id("password_pwd")
             print('Searching connect')
-            login_button = driver.find_element_by_id("password_submitBtn")  # 校园网登陆连接的点击控件ID, 浏览器上右键查看网页源代码查询
+            login_button = driver.find_element_by_id("password_submitBtn")
             print('Find connect successfully')
             username_input.send_keys(username_str)
             password_input.send_keys(password_str)
